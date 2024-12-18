a4f75dfe1faea512506fede66c3a253504954214
mahdavi <mahdavi@sahab.ir>
2024-12-18 11:15:53 +0330

    refactor/App

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,30 @@
+<script setup>
+import baseCard from "@/components/base/card.vue";
+
+const images = Array.from({ length: 8 }, (_, index) =>
+  {
+    return {
+      src:`/src/assets/images/product-${index + 1}.jpg`,
+      isFliped:false,
+    }
+  }
+)
+</script>
+
 <template>
   <v-app>
     <v-main>
-      <HelloWorld />
+      <v-container>
+        <v-row>
+          <v-col
+            cols="3"
+            v-for="(image, index) in images"
+            :key="index"
+          >
+            <base-card :img="image.src" :index="index+1" :isFliped="image.isFliped" @click="image.isFliped = !image.isFliped"/>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
-
-    <AppFooter />
   </v-app>
 </template>
-
-<script setup>
-  //
-</script>
